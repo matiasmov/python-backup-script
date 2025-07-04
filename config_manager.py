09674c0c9b4be4c9dfc6f2fe779b30379e0f62f0
Lucas Matias <lucasmatias2005@gmail.com>
2025-07-04 13:48:11 -0300

    fix:bug: Corrige erro na validação e carregamento da env

--- config_manager.py
+++ config_manager.py
@@ -2,7 +2,12 @@
 import os
 from dotenv import load_dotenv
 
+
+# Carrega as env
+
 load_dotenv()
+
+# Colocando na variável, não precisa ficar repetindo os.getenv
 
 PATH = os.getenv("BACKUP_PATH")
 API_BASE_URL = os.getenv("API_BASE_URL")
